c5d47fbf6e6d53f8628bdd5cdc27500381a367a2
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-10-04 15:59:09 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,52 +4,90 @@ class Profile {
         this.age = age;
         this.city = city;
         this.photo = photo;
+        this.liked = 0;
     }
 }
 
+let profile;
+let profilekey = 0;
+let clickcount = 0;
+
+function toLocalStorage(name, values) {
+    localStorage.setItem(name, values);
+}
+
+function getLocalStorage(key) {
+    return localStorage.getItem(key);
+}
+
 function getData() {
-    let arrayPeople = [];
     fetch('https://randomuser.me/api/?results=10').then(function(response) {
         return response.json();
     }).then(function(data) {
-        let people = data.results;
-        for(let i = 0; i < people.length; i++) {
-            let person = new Profile(people[i].name.first, people[i].dob.age, people[i].location.city, people[i].picture.large);
-            arrayPeople.push(JSON.stringify(person));
+        console.log(data.results);
+        console.log(profilekey);
+        for(let i = 0; i < data.results.length; i++) {
+            // stringArray.push(JSON.stringify(data.results[i]));
+            let person = new Profile(data.results[i].name.first, data.results[i].dob.age, data.results[i].location.city, data.results[i].picture.large);
+            toLocalStorage(i + profilekey, JSON.stringify(person));
+            
         }
-        console.log(arrayPeople);
-        localStorage.setItem('people', arrayPeople);
-    })
+        // toLocalStorage('people', stringArray);
+    });
 }
 
-function getProfiles() {
-    // fetch('https://randomuser.me/api/').then(function(response) {
-    //     response.json().then(function(data) {
-    //         console.log(data);
+function getProfile(key) {
+    let profdata = JSON.parse(getLocalStorage(key));
+    document.getElementById('profilearea').innerHTML = `
+    <div class='profile'>
+        <div class='profile__image'>
+            <img src='${profdata.photo}' alt='${profdata.name}' class='picture__fill'>
+        </div>
+        <div class='profile__info'>
+            <p><span class='profile__name'>${profdata.name}</span><span class='profile__age'>${profdata.age}</span></p>
+            <p class='profile__home'>${profdata.city}</p>
+        </div>
+    </div>
+    `;
+    console.log(profdata);
+    return profdata;
+}
 
-    //         document.getElementById('profilearea').innerHTML = `
-    //         <div class='profile'>
-    //             <div class='profile__image'>
-    //                 <img src='${data.results[0].picture.large}' alt='' class='profile__picture'>
-    //             </div>
-    //             <div class='profile__info'>
-    //                 <p><span class='profile__name'>${data.results[0].name.first}</span><span class='profile__age'>${data.results[0].dob.age}</span></p>
-    //                 <p class='profile__home'>${data.results[0].location.city}</p>
-    //             </div>
-    //         </div>
-    //         `;
-    //     });
-    // }, function(error) {
-    //     console.log('ERROR: ' + error);
-    // });
+function refreshData() {
+    document.getElementById('profilearea').innerHTML = '';
+    clickcount = 0;
     getData();
-    let localData = localStorage.getItem('people');
-    console.log(localData);
-    let splitstring = localData.split(',');
-    let parseArray = [];
-    forEach(splitstring => {
-        
-    })
+    console.log(profilekey);
+    profile = getProfile(profilekey);
 }
 
-getProfiles();
+function like() {
+    console.log('Liked:' + profile.name);
+    profile.liked = 1;
+    toLocalStorage(profilekey, JSON.stringify(profile));
+
+    if(clickcount > 9) {
+        refreshData();
+    }
+    else {
+        profilekey++;
+        profile = getProfile(profilekey);
+    }
+}
+
+function dislike() {
+
+}
+
+
+// Onload
+window.onload = refreshData();
+
+// event listeners
+let likebutton = document.getElementById('yes');
+let dislikebutton = document.getElementById('nope');
+likebutton.addEventListener('click', function() {like()});
+dislikebutton.addEventListener('click', dislike());
+
+
+
